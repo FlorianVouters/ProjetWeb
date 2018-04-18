8af530a51e84976bc68b3ba5e154c1dc76b62c27
Spechen <florian.vouters@viacesi.fr>
2018-04-19 01:43:00 +0200

    Missing Form to Account

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -1,32 +1,30 @@
 <?php
-// /src/Form/UserType.php
+
 namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('username', TextType::class)
             ->add('firstname', TextType::class)
-            ->add('email', EmailType::class)            
             ->add('surname', TextType::class)
-            ->add('password', PasswordType::class)
+            ->add('email', EmailType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'data_class' => User::class,
-        ));
+        ]);
     }
 }
