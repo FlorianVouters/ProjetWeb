69c8aacd44feab4adc1aa0b099b579118a74ce66
Spechen <florian.vouters@viacesi.fr>
2018-04-19 01:40:17 +0200

    Adding Panel Account with Edit

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,18 +2,54 @@
 
 namespace App\Controller;
 
-use http\Env\Response;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Twig\Environment;
 
+/**
+ * @Route("/account")
+ * Class AccountController
+ * @package App\Controller
+ */
 class AccountController extends Controller
 {
     /**
-     * @Route("/account", name="account")
+     * @Route("/", name="account")
      */
     public function index(Environment $twig)
     {
-        return new Response($twig->render('account/account.twig.html'));
+        $user = $this->getUser();
+        if(!$user) throw $this->createNotFoundException('Not Connected');
+
+
+        return new Response($twig->render('account/account.html.twig', ['user' => $user]));
+    }
+
+
+
+    /**
+     * @Route("/edit", name="user_edit", methods="GET|POST")
+     */
+    public function edit(Request $request)
+    {
+        $user = $this->getUser();
+        if(!$user) throw $this->createNotFoundException('Not Connected');
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_edit');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
     }
 }
