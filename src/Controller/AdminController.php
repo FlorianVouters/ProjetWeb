3bb850b182d4bdba1fb90d39f5cd45c3275382e3
FlorianVouters <florian.vouters@viacesi.fr>
2018-04-18 16:16:07 +0200

    Admin update

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,45 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Form\CategoryType;
+use App\Form\ProductType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Twig\Environment;
 
+
+/**
+ * Class AdminController
+ * @Route("/admin")
+ * @package App\Controller
+ */
 class AdminController extends Controller
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="admin")
      */
     public function index()
     {
         return $this->render('Admin/index.html.twig');
     }
 
-    /**
- * @Route("/admin/suggestions", name="adminSuggestion")
- */
-    public function adminSuggestion(Environment $twig)
-    {
-        return new Response($twig->render('Admin/suggestion.html.twig'));
-    }
-
-    /**
-     * @Route("/admin/activity/edit", name="adminSuggestion")
-     */
-    public function editSuggestion(Environment $twig, Request $request)
-    {
-        $session = new Session();
-        $id = $request->attributes->get('id');
-        if(!$id) {
-            $session->getFlashBag()->set('error', 'Aucun ID spécifié lors de l\'édition.');
-            return new Response($twig->render('suggestion/index.html.twig'));
-        }
 
 
 
-        return new Response($twig->render('Admin/editSuggestion.html.twig'));
-    }
+
 }
