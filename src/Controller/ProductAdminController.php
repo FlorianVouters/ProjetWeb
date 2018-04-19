c5818e15f20cc6aeb411f731cb78c9fd9e8db74b
FlorianVouters <florian.vouters@viacesi.fr>
2018-04-19 17:31:12 +0200

    Update Product

--- src/Controller/ProductAdminController.php
+++ src/Controller/ProductAdminController.php
@@ -20,7 +20,7 @@ class ProductAdminController extends Controller
      */
     public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('product/index.html.twig', ['products' => $productRepository->findAll()]);
+        return $this->render('admin/product/index.html.twig', ['products' => $productRepository->findAll()]);
     }
 
     /**
@@ -34,7 +34,7 @@ class ProductAdminController extends Controller
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $product->setSells(0);
+            $product->setSold(0);
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($product);
@@ -89,5 +89,64 @@ class ProductAdminController extends Controller
         }
 
         return $this->redirectToRoute('admin/product_index');
+    }
+
+
+    public function setProduct($nom, $description, $prix, $categorie){
+        $entityManager = $this->getDoctrine()->getManager();
+        $produit = new Product();
+
+        $produit->setName($nom);
+        $produit->setDescription($description);
+        $produit->setPrice($prix);
+        $produit->setCategoryId($categorie);
+        $produit->setSold('0');
+
+        $entityManager->persist($produit);
+        $entityManager->flush();
+    }
+
+    public function getProduct($id){
+
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+        $produit = $repository->find($id);
+        return $produit ;
+    }
+
+    public function addProduct($nom, $description, $prix, $categorie){ //Suppression de "$nombreVente" car non besoin
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $activity = new Product();
+
+        $activity->setName($nom);
+        $activity->setDescription($description);
+        $activity->setPrice($prix);
+        $activity->setCategoryId($categorie);
+        $activity->setSold( 0);
+        $entityManager->persist($activity);
+        $entityManager->flush();
+    }
+
+    public function deleteProduct($product_id){
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $produit = $repository->findOneBy([
+            'product_id' => $product_id,
+        ]);
+        $entityManager->remove($produit);
+        $entityManager->flush();
+    }
+
+    public function productSold($product_id){
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $produit = $repository->findOneBy([
+            'product_id' => $product_id,
+        ]);
+        $produit->nombreVente = $produit->nombreVente =1;
+        $entityManager->persist($produit);
+        $entityManager->flush();
     }
 }
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -8,6 +8,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,11 +18,11 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price', MoneyType::class)
-            ->add('image', new MediaType())
-            ->add('category', EntityType::class, array(
+            ->add('name', TextType::class , array('label' => 'Nom de produit'))
+            ->add('description', TextareaType::class, array('label' => 'Description du produit'))
+            ->add('price', MoneyType::class, array('label' => 'Prix'))
+            ->add('image', MediaType::class, array('label' => 'Image du produit'))
+            ->add('category_id', EntityType::class, array(
                                         'class' => Category::class,
                                         'choice_label' => function($category) {
                                                     return $category->getName();
