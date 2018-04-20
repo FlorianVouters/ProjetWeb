c2f6a944dbccb528b3d0051e5216289760e73214
FlorianVouters <florian.vouters@viacesi.fr>
2018-04-20 08:42:57 +0200

    Update Package Encore

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+// the project directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    // uncomment to create hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // uncomment to define the assets of the project
+    .addEntry('js/bootstrapBundle', './assets/js/bootstrap.bundle.js')
+    .addEntry('js/creation', './assets/js/creation.js')
+    .addEntry('js/buttonTrigger', './assets/js/buttonTrigger.js')
+
+    .addStyleEntry('css/style', './assets/css/style.css')
+    .addStyleEntry('css/bootstrap', './assets/css/bootstrap.css')
+
+    // uncomment if you use Sass/SCSS files
+    .enableSassLoader()
+
+// uncomment for legacy applications that require $/jQuery as a global variable
+// .autoProvidejQuery()
+;
+
+module.exports = Encore.getWebpackConfig();
