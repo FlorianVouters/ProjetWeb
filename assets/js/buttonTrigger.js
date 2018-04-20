2aa74fbf21b9d636b5fafd5ad349f63137e106e3
FlorianVouters <florian.vouters@viacesi.fr>
2018-04-20 08:41:39 +0200

    Adding assets file

---
+++ assets/js/buttonTrigger.js
@@ -0,0 +1,19 @@
+var xhr = new XMLHttpRequest();
+
+$("#like").click((e) => {
+    // alert("Type : comment ; ID : " + .parent().attr(id));
+    alert(document.getElementById("like").par().attr(id));
+
+
+
+    // e.preventDefault();
+    // $.ajax({
+    //     type: "POST",
+    //     url: "",
+    //     data: {
+    //         id: $(this.parentElement.attr('id'))
+    //     }
+    //
+    // })
+});
+
